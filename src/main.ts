3a47177e981dde2f0e741657de45e7f66f6bd5c3
juancalero <marcelocalero5@gmail.com>
2023-08-20 18:50:16 -0300

    push

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const logger = new Logger('bootstrap');
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.setGlobalPrefix('api');
+  await app.listen(3000);
+  logger.log(`Application listening on port ${await app.getUrl()}/api`);
+}
+bootstrap();
