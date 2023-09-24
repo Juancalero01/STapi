17ce3d17660bd8473200c8bb63e8f7bdf0f5417b
juancalero <marcelocalero5@gmail.com>
2023-09-24 15:07:33 -0300

    push client

--- src/modules/client/dto/update-client.dto.ts
+++ src/modules/client/dto/update-client.dto.ts
@@ -1,10 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateClientDto } from './create-client.dto';
-import { StateEnum } from 'src/shared/enums/state.enum';
-import { IsEnum, IsOptional } from 'class-validator';
+import { IsBoolean, IsOptional } from 'class-validator';
 
 export class UpdateClientDto extends PartialType(CreateClientDto) {
   @IsOptional({ message: 'State is optional' })
-  @IsEnum(StateEnum, { message: 'State must be a valid enum value' })
-  state?: StateEnum;
+  @IsBoolean({ message: 'State must be a boolean' })
+  state?: boolean;
 }
--- src/modules/client/entity/client.entity.ts
+++ src/modules/client/entity/client.entity.ts
@@ -1,11 +1,13 @@
 import { ProductEntity } from 'src/modules/product/entity/product.entity';
 import { BaseEntity } from 'src/shared/entities/base.entity';
-import { Entity, Column, OneToMany } from 'typeorm';
+import { ProvinceEntity } from 'src/shared/modules/province/province.entity';
+import { TaxConditionEntity } from 'src/shared/modules/tax-condition/tax-condition.entity';
+import { Entity, Column, OneToMany, ManyToOne } from 'typeorm';
 
 @Entity('clients')
 export class ClientEntity extends BaseEntity {
-  @Column({ type: 'varchar', length: 120, unique: true, nullable: false })
-  name: string;
+  @Column({ type: 'varchar', length: 150, unique: true, nullable: false })
+  taxpayerName: string;
 
   @Column({
     type: 'varchar',
@@ -14,10 +16,13 @@ export class ClientEntity extends BaseEntity {
     nullable: true,
     default: null,
   })
-  cuit: string;
+  taxpayerId: string;
 
-  @Column({ type: 'varchar', length: 120, nullable: true, default: null })
-  contact: string;
+  @ManyToOne(() => TaxConditionEntity, (taxCondition) => taxCondition.clients, {
+    nullable: true,
+    cascade: true,
+  })
+  taxCondition: TaxConditionEntity;
 
   @Column({
     type: 'varchar',
@@ -26,7 +31,7 @@ export class ClientEntity extends BaseEntity {
     nullable: true,
     default: null,
   })
-  email: string;
+  taxpayerEmail: string;
 
   @Column({
     type: 'char',
@@ -35,7 +40,52 @@ export class ClientEntity extends BaseEntity {
     nullable: true,
     default: null,
   })
-  phone: string;
+  taxpayerPhone: string;
+
+  @ManyToOne(() => ProvinceEntity, (province) => province.clients, {
+    nullable: true,
+    cascade: true,
+  })
+  province: ProvinceEntity;
+
+  @Column({ type: 'varchar', length: 150, nullable: true, default: null })
+  street: string;
+
+  @Column({ type: 'varchar', length: 120, nullable: true, default: null })
+  number: string;
+
+  @Column({ type: 'varchar', length: 120, nullable: true, default: null })
+  floor: string;
+
+  @Column({ type: 'varchar', length: 50, nullable: true, default: null })
+  office: string;
+
+  @Column({ type: 'varchar', length: 8, nullable: true, default: null })
+  postalCode: string;
+
+  @Column({ type: 'varchar', length: 120, nullable: true, default: null })
+  contactName: string;
+
+  @Column({
+    type: 'varchar',
+    length: 120,
+    unique: true,
+    nullable: true,
+    default: null,
+  })
+  contactEmail: string;
+
+  @Column({
+    type: 'char',
+    length: 20,
+    unique: true,
+    nullable: true,
+    default: null,
+  })
+  contactPhone: string;
+
+  @Column({ type: 'varchar', length: 250, nullable: true, default: null })
+  comment: string;
 
   @OneToMany(() => ProductEntity, (product) => product.client)
   products: ProductEntity[];
