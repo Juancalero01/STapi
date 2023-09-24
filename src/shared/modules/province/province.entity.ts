9033f24dc9787367fe6fc4d5279d2168c6d1d04b
juancalero <marcelocalero5@gmail.com>
2023-09-24 14:55:18 -0300

    updated modules with client

--- src/shared/modules/province/province.entity.ts
+++ src/shared/modules/province/province.entity.ts
@@ -1,8 +1,12 @@
+import { ClientEntity } from 'src/modules/client/entity/client.entity';
 import { BaseEntity } from 'src/shared/entities/base.entity';
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, OneToMany } from 'typeorm';
 
 @Entity('provinces')
 export class ProvinceEntity extends BaseEntity {
   @Column({ type: 'varchar', length: 150, unique: true, nullable: false })
   name: string;
+
+  @OneToMany(() => ClientEntity, (client) => client.province)
+  clients: ClientEntity[];
 }
