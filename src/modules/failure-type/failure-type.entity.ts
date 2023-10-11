e00fd1978719e69879e0ca5e7f202d7a605db1dd
juancalero <marcelocalero5@gmail.com>
2023-10-11 08:27:39 -0300

    push

--- src/modules/failure-type/failure-type.entity.ts
+++ src/modules/failure-type/failure-type.entity.ts
@@ -1,8 +1,12 @@
 import { BaseEntity } from 'src/shared/entities/base.entity';
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, OneToMany } from 'typeorm';
+import { ServiceEntity } from '../service/service.entity';
 
 @Entity('failure_types')
 export class FailureTypeEntity extends BaseEntity {
   @Column({ type: 'varchar', length: 60, nullable: false })
   name: string;
+
+  @OneToMany(() => ServiceEntity, (service) => service.failureType)
+  service: ServiceEntity;
 }
--- src/modules/service/service.entity.ts
+++ src/modules/service/service.entity.ts
@@ -3,28 +3,52 @@ import { BaseEntity } from 'src/shared/entities/base.entity';
 import { ServiceStateEntity } from 'src/modules/service-state/service-state.entity';
 import { Column, Entity, ManyToOne } from 'typeorm';
 import { ServicePriorityEntity } from '../service-priority/service-priority.entity';
+import { FailureTypeEntity } from '../failure-type/failure-type.entity';
 
 @Entity('services')
 export class ServiceEntity extends BaseEntity {
-  @Column({ type: 'varchar', nullable: false })
-  reclaim: string;
-  @Column({ type: 'varchar', length: 255, nullable: true, default: null })
-  failure: string;
-  @Column({ type: 'varchar', length: 255, nullable: true, default: null })
-  reference: string;
-  @Column({ type: 'varchar', length: 255, nullable: false })
-  remarks: string;
+  // fecha de ingreso del servicio
   @Column({ type: 'date', nullable: false })
   dateEntry: Date;
-  @Column({ type: 'boolean', nullable: true, default: false })
-  warranty: boolean;
-  @ManyToOne(() => ProductEntity, (product) => product.service)
-  product: ProductEntity;
+
+  // numero de reclamo del producto a reparar
+  @Column({ type: 'varchar', nullable: false })
+  reclaim: string;
+
+  // Estado del servicio
   @ManyToOne(() => ServiceStateEntity, (serviceState) => serviceState.service)
   state: ServiceStateEntity;
+
+  // Producto asociado al servicio
+  @ManyToOne(() => ProductEntity, (product) => product.service)
+  product: ProductEntity;
+
+  // Prioridad del servicio
   @ManyToOne(
     () => ServicePriorityEntity,
     (servicePriority) => servicePriority.service,
   )
   priority: ServicePriorityEntity;
+
+  // remito del producto a reparar
+  @Column({ type: 'varchar', length: 255, nullable: true, default: null })
+  reference: string;
+
+  @Column({ type: 'boolean', nullable: true, default: false })
+  securityStrap: boolean;
+
+  // falla del producto a reparar
+  @Column({ type: 'varchar', length: 255, nullable: true, default: null })
+  failure: string;
+
+  @ManyToOne(() => FailureTypeEntity, (failureType) => failureType.service)
+  failureType: FailureTypeEntity;
+
+  // observaciones del servicio
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  remarks: string;
+
+  // garantia del servicio
+  @Column({ type: 'boolean', nullable: true, default: false })
+  warranty: boolean;
 }
