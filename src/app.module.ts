d43251e3e1c1ce40763e7d0fbc8dd5f5c62e29a8
juancalero <marcelocalero5@gmail.com>
2023-11-21 08:17:37 -0300

    push

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ import { AuthModule } from './modules/auth/auth.module';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'root',
+      password: 'CNET2023',
       database: 'cnet',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
