f139c1f151d63aded2b1446e16243e29b516d4b0
juancalero <marcelocalero5@gmail.com>
2023-11-27 20:17:24 -0300

    push

--- src/modules/role/common/role.guard.ts
+++ src/modules/role/common/role.guard.ts
@@ -7,11 +7,11 @@ export class RolesGuard implements CanActivate {
 
   canActivate(context: ExecutionContext): boolean {
     const roles = this.reflector.get<string[]>('roles', context.getHandler());
-    if (!roles) {
-      return true;
-    }
     const request = context.switchToHttp().getRequest();
     const user = request.user;
+    if (user && user.role === 'ADMINISTRADOR') {
+      return true;
+    }
     return user && user.role && roles.includes(user.role);
   }
 }
