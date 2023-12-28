a5b6508ba52ff8e9c7b5998b39a5718eab79097e
juancalero <marcelocalero5@gmail.com>
2023-12-28 08:05:33 -0300

    push

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,7 +21,7 @@ import { RoleModule } from './modules/role/role.module';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'root',
+      password: 'CNET2023',
       database: 'cnet',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
